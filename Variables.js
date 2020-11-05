263d19426a2e0367ecf9ef2f999664fcb27c7084
Yamini Kamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2020-11-05 12:15:37 +0530

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -25,3 +25,47 @@ A = a + 2; // we can do arithmetic operation using variables
 
 var name = "Paul", age = 20, country = "India"; // Can declare multiple variables in a single line
 
+// Declaring number using Number Wrapper object
+var k=Number(98);
+console.log(k); //returns 98
+console.log(typeof k); //returns number
+
+k=Number(true);
+console.log(k); //returns 1
+k=Number(true+1);
+console.log(k); //returns 2
+
+k=Number("Hi"+1);
+console.log(k); //returns NaN
+
+k=Number(true+"Hi");
+console.log(k); //returns NaN
+
+k=Number(true+"true");
+console.log(k);
+
+k=Number(true+null);
+console.log(k);  //returns 1
+
+k=Number(null+null);
+console.log(k); //returns 0
+
+k=Number(null+undefined);
+console.log(k); //returns NaN
+
+k=Number(""+1);
+console.log(k); //returns 1
+
+a=Number(10n);
+console.log(a) //returns 10
+console.log(typeof a);  //returns number
+
+// declaring string using String wrapper object
+var h=String("hi");
+console.log(h) //returns "hi"
+console.log(typeof h); //returns string
+
+var k= new String("string obj");
+console.log(k) // returns object {"string obj"}
+console.log(typeof k);  //returns object
+
