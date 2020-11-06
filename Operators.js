b57cbed80ac6cb7582cb5b883b3dbe29c87a4ab1
Yamini Kamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2020-11-06 15:54:21 +0530

    Update Operators.js

--- Operators.js
+++ Operators.js
@@ -24,6 +24,108 @@ y**=true+1; //returns 100
 var j=6;
 j+="true"; // returns string "6true" -->concatenation 
 
+//Basic math opereators
+// + Addition
+
+console.log(1+1); //returns 2
+console.log(1+"1"); //returns 11
+console.log("1"+1); //returns 11
+console.log(true+1); //returns 2
+console.log(true+""); //returns "true"
+console.log(true+"hi"); //returns "truehi"
+console.log(true+null); //returns 1
+console.log(true+undefined); //returns NaN
+console.log(true+"undefined"); //returns "trueundefined"
+console.log(true+true); //returns 2
+console.log(8+"\n"); //returns "8"
+console.log(null+"null"); //returns "nullnull"
+console.log(null+undefined); //returns NaN
+console.log(true+"true") //returns "truetrue"
+
+// - Subtraction
+console.log(1-2); // returns -1
+x= true-true; //x=0
+x=true-"true"; //x=NaN
+x=1-true; //x=0
+x=true-null; //x=1
+x=true-undefined; //x=NaN
+x=true-"1"; //returns 0
+x=9-"2"; //x=7
+x=9-null; //x=9
+x=9-undefined; //x=undefined
+x=""-9; //x=-9
+x="10"-9; //x=1
+
+// * Multiplication
+x=2*2; //x=4
+x=2*""; //x=0
+x="5"*0; //x=0
+x="5"*null; //x=0
+x="5"*undefined; //x=NaN
+x=null*"5"; //x=0
+x="2"*"5"; //x=10
+x=5*"\n"; //x=0
+x=5*"\n8"; //x=40
+x="true"*"\n8"; //x=NaN
+x="true"*6; //x=NaN
+x=6*true+1; //x=7
+x=6+true*true+1; //x=8
+x=6+true-true+1-"3"; //x=4
+x=6+true-true+1-"3"*"7"; //x=-14
+
+// / Division
+x=7/2; //3.5
+x=8/true; //8
+x=8/"true"; //NaN
+x=9/null; //Infinity
+x=9/false; //Infinity
+x=7/undefined; //NaN
+x=7/""; //Infinity
+x=""/9; //0
+x="18"/9; //2
+x="true"/9; //NaN
+x="\n27"/3; //9
+x="false"/3; //NaN
+
+// % Remainder
+x=12%2; // 0
+45%2; //1
+33%4; //1
+true%5; //1
+44%0 //NaN
+44%""//NaN
+44%null//NaN
+null%null;//NaN
+null%5;//0
+null%undefined;//NaN
+undefined % null;//NaN
+null%"";//NaN
+11n%10n; //1n
+"true"%8; //NaN
+"a"%"b";//NaN
+
+// ** Exponent
+2**3;//8
+2**true;//2
+2**true+true;//3
+2**0;//1
+0**5;//0
+""**2;//0
+"9"**2;//81
+"9"**"2";//81
+"9"**"true";//NaN
+"9"**"";//1
+"9"**null;//1
+"9"**undefined;//NaN
+null**7;//0
+undefined**null;//1
+null**undefined;//NaN
+""**null;//1
+true**null;//1
+true**undefined*null;//NaN
+true**undefined**null; //1
+true*undefined**null //1
+
 //Comparasion operator
 
 console.log(7=="7");   //returns true --> compares only the value
